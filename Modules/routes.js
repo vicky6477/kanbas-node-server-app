4e55c3c5379bbe0cc93e73fae753fda98fe5c0b3
vicky <vicky64777@gmail.com>
2023-11-20 13:41:50 -0800

    first commit

---
+++ Modules/routes.js
@@ -0,0 +1,36 @@
+import db from "../Database/index.js";
+function ModuleRoutes(app) {
+    app.put("/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        const moduleIndex = db.modules.findIndex((m) => m._id === mid);
+        db.modules[moduleIndex] = {
+            ...db.modules[moduleIndex],
+            ...req.body,
+        };
+        res.sendStatus(204);
+    });
+
+    app.delete("/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        db.modules = db.modules.filter((m) => m._id !== mid);
+        res.sendStatus(200);
+    });
+
+    app.post("/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params;
+        const newModule = {
+            ...req.body,
+            course: cid,
+            _id: new Date().getTime().toString(),
+        };
+        db.modules.push(newModule);
+        res.send(newModule);
+    });
+
+    app.get("/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params;
+        const modules = db.modules.filter((m) => m.course === cid);
+        res.send(modules);
+    });
+}
+export default ModuleRoutes;
---
+++ app.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import session from "express-session";
+import express from 'express'
+import Hello from "./hello.js"
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./Courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import cors from "cors";
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app)
+app.listen(process.env.PORT || 4000);
