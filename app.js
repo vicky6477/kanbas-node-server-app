318d0a9086c652c7da9197594859ac39a50fbbea
vicky <vicky64777@gmail.com>
2023-12-04 16:13:10 -0800

    a6

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import UserRoutes from "./users/routes.js";
 
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
---
+++ users/routes.js
@@ -0,0 +1,31 @@
+import * as dao from "./dao.js";
+let currentUser = null;
+function UserRoutes(app) {
+    const createUser = async (req, res) => {};
+    const deleteUser = async (req, res) => {};
+    const findAllUsers = async (req, res) => {};
+    const findUserById = async (req, res) => {};
+    const updateUser = async (req, res) => {};
+    const signup = async (req, res) => {};
+    const signin = async (req, res) => {
+        const { username, password } = req.body;
+        currentUser = await dao.findUserByCredentials(username, password);
+        res.json(currentUser);
+    };
+
+    const signout = (req, res) => {};
+    const account = async (req, res) => {
+        res.json(currentUser);
+    };
+
+    app.post("/users", createUser);
+    app.get("/users", findAllUsers);
+    app.get("/users/:userId", findUserById);
+    app.put("/users/:userId", updateUser);
+    app.delete("/users/:userId", deleteUser);
+    app.post("/users/signup", signup);
+    app.post("/users/signin", signin);
+    app.post("/users/signout", signout);
+    app.post("/users/account", account);
+}
+export default UserRoutes;
