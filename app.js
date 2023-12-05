a270b47ab5dd9b08d92d5e3deefc16dd3b695695
vicky <vicky64777@gmail.com>
2023-12-04 19:56:42 -0800

    a6 extra credit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
-import express from 'express'
-import Hello from "./hello.js"
+import express from 'express';
+import "dotenv/config";
+import session from "express-session";
+import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
@@ -9,12 +11,27 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import UserRoutes from "./users/routes.js";
 
-const app = express()
-app.use(cors());
+const app = express();
+app.use(cors({ credentials: true, origin: "https://a6--luminous-kangaroo-c0e365.netlify.app/#/project" }));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
-Hello(app)
+Hello(app);
 app.listen(process.env.PORT || 4000);
--- users/routes.js
+++ users/routes.js
@@ -1,5 +1,5 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+
 function UserRoutes(app) {
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
@@ -24,7 +24,8 @@ function UserRoutes(app) {
     const updateUser = async (req, res) => {
         const { userId } = req.params;
         const status = await dao.updateUser(userId, req.body);
-        currentUser = await dao.findUserById(userId);
+        const currentUser = await dao.findUserById(userId);
+        req.session["currentUser"] = currentUser;
         res.json(status);
     };
 
@@ -33,23 +34,25 @@ function UserRoutes(app) {
         if (user) {
             res.status(400).json({ message: "Username already taken" });
         }
-        currentUser = await dao.createUser(req.body);
+        const currentUser = await dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
 
     const signin = async (req, res) => {
         const { username, password } = req.body;
-        currentUser = await dao.findUserByCredentials(username, password);
+        const currentUser = await dao.findUserByCredentials(username, password);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
 
     const signout = (req, res) => {
-        currentUser = null;
+        req.session.destroy();
         res.json(200);
     };
 
     const account = async (req, res) => {
-        res.json(currentUser);
+        res.json(req.session["currentUser"]);
     };
 
     app.post("/users", createUser);
