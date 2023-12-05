8dd1fe71c6de2ff779790803fecf3e6653deea24
vicky <vicky64777@gmail.com>
2023-12-05 15:47:03 -0800

    a6 add netlify env

--- app.js
+++ app.js
@@ -8,19 +8,15 @@ import ModuleRoutes from "./Modules/routes.js";
 import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 
-
+// mongoose.connect("mongodb://27017/kanbas");
 const app = express();
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL || "http://localhost:3000"
- }));if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
-}
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL
+}));
 
 
 const sessionOptions = {
@@ -28,6 +24,14 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(session(sessionOptions));
 
 app.use(express.json());
