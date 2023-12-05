8492b53f467ee44bbefe6c1a0bb0c67779685250
vicky <vicky64777@gmail.com>
2023-12-04 22:18:02 -0800

    a6 atlas

--- app.js
+++ app.js
@@ -8,27 +8,17 @@ import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-console.log("Connecting to DB with string:", CONNECTION_STRING);
-mongoose.connect(CONNECTION_STRING);
-
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import UserRoutes from "./users/routes.js";
 
 const app = express();
-app.use(cors({ credentials: true, origin: "https://a6--luminous-kangaroo-c0e365.netlify.app/#/project/signin" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
-};if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
-}
-
+};
 app.use(session(sessionOptions));
 
 app.use(express.json());
