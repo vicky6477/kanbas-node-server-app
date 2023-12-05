2bf8239d18978a1752b0ab6f729353879364d7d2
vicky <vicky64777@gmail.com>
2023-12-05 00:00:34 -0800

    a6 add netlify env

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import "dotenv/config";
 import express from 'express';
-import "dotenv/config";
 import session from "express-session";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-import UserRoutes from "./users/routes.js";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL || "http://localhost:3000"
+ }));if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 
 const sessionOptions = {
     secret: "any string",
--- users/routes.js
+++ users/routes.js
@@ -1,5 +1,4 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 function UserRoutes(app) {
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
@@ -21,36 +20,46 @@ function UserRoutes(app) {
         res.json(user);
     };
 
-    const updateUser = async (req, res) => {
-        const { userId } = req.params;
-        const status = await dao.updateUser(userId, req.body);
-        currentUser = await dao.findUserById(userId);
-        res.json(status);
-    };
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    const currentUser = await dao.findUserById(userId);
+    req.session['currentUser'] = currentUser;
+    res.json(status);
+  };
 
-    const signup = async (req, res) => {
-        const user = await dao.findUserByUsername(req.body.username);
-        if (user) {
-            res.status(400).json({ message: "Username already taken" });
-        }
-        currentUser = await dao.createUser(req.body);
-        res.json(currentUser);
-    };
 
-    const signin = async (req, res) => {
-        const { username, password } = req.body;
-        currentUser = await dao.findUserByCredentials(username, password);
-        res.json(currentUser);
-    };
+    
 
-    const signout = (req, res) => {
-        currentUser = null;
-        res.json(200);
-    };
+ const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(
+      req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already taken" });
+    }
+    const currentUser = await dao.createUser(req.body);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser);
+  };
 
-    const account = async (req, res) => {
-        res.json(currentUser);
-    };
+
+   const signin = async (req, res) => {
+    const { username, password } = req.body;
+    const currentUser = await dao.findUserByCredentials(username, password);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser);
+  };
+
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.json(200);
+  };
+
+   const account = async (req, res) => {
+    res.json(req.session['currentUser']);
+  };
+
 
     app.post("/users", createUser);
     app.get("/users", findAllUsers);
