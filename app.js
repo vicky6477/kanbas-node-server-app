59c5c986790e7d216dccb460580904bb13acc1ce
vicky <vicky64777@gmail.com>
2023-12-05 16:09:58 -0800

    a6 add netlify env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 
-// mongoose.connect("mongodb://27017/kanbas");
+
 const app = express();
 app.use(cors({
   credentials: true,
